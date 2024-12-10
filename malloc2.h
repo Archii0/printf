92b9b1061f6937461e8db0bcdde2e9ed4098ce68
Archii0 <archiedascombe@icloud.com>
2024-12-10 01:42:25 +0000

    implemented naive malloc

---
+++ malloc2.h
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+#define malloc2(x)                                                             \
+    sbrk(x);                                                                   \
+    _alloc += x;
+#define freeall()                                                              \
+    sbrk(-_alloc);                                                             \
+    _alloc = 0;
+
+unsigned int _alloc;
